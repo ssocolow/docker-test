ebc1d7177746a2db93253f1b828525448b922131
Simon Socolow <simonsocolow78@gmail.com>
2024-11-09 23:56:41 +0000

    streak info

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/info')
 def home():
-    return "skldfskdj fal;! Hello, Docker!"
+    streakInfo = {"current_streak": 6, "longest_streak": 10, "total_uploads": 23, "last_upload": "Yesterday"}
+    return streakInfo
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
